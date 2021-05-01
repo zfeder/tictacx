30d5eb441d3a1ebf1f0bcb41f12fac3da0948732
Federico Filì <32766721+federicofli@users.noreply.github.com>
2021-05-01 18:32:09 +0200

    Spawn Button Input

--- TicTacx/main.py
+++ TicTacx/main.py
@@ -1,43 +1,35 @@
-from _ast import Lambda
 from tkinter import *
-from tkinter import messagebox
 
-window = Tk()
-window.title("Filetto")
-window.geometry("800x800")
+root = Tk()
+root.title("Filetto")
+root.geometry("700x700")
+
+my_entries = []
+
+z = input("Inserisci il valore: ")
+w = int(z)
 
 
-#Button Click
 def b_click(b):
     pass
 
 
-#Build Buttons
-b1 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b1))
-b2 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b2))
-b3 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b3))
+def buttons():
+    entry_list = ''
 
-b4 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b4))
-b5 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b5))
-b6 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b6))
+    for entries in my_entries:
+        entry_list = entry_list + str(entries.get()) + '\n'
 
-b7 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b7))
-b8 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b8))
-b9 = Button(window, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace", command=lambda: b_click(b9))
+    print(my_entries[24].get())
 
 
-#Grid Buttons to the screen
-b1.grid(row=0, column=0)
-b2.grid(row=0, column=1)
-b3.grid(row=0, column=2)
+# Row Loop
+for y in range(w):
+    # Column Loop
+    for x in range(w):
+        my_entry = Button(root, text="ciao", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                          command=lambda: b_click(my_entry))
+        my_entry.grid(row=y, column=x)
+        my_entries.append(my_entry)
 
-b4.grid(row=1, column=0)
-b5.grid(row=1, column=1)
-b6.grid(row=1, column=2)
-
-b7.grid(row=2, column=0)
-b8.grid(row=2, column=1)
-b9.grid(row=2, column=2)
-
-
-window.mainloop()
+root.mainloop()

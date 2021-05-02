fc021c8131e4f2b0478bcc89503220b72532f675
Federico Filì <32766721+federicofli@users.noreply.github.com>
2021-05-02 16:59:50 +0200

    Button X or O

--- TicTacx/main.py
+++ TicTacx/main.py
@@ -1,8 +1,12 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
 root.title("Filetto")
 root.geometry("700x700")
+
+clicked = True
+count = 0
 
 my_entries = []
 
@@ -11,10 +15,25 @@ w = int(z)
 
 
 def b_click(b):
-    pass
+    global clicked, count
+
+    if b["text"] == " " and clicked == True:
+        b["text"] = "X"
+        clicked = False
+        count += 1
+    elif b["text"] == " " and clicked == False:
+        b["text"] = "O"
+        clicked = True
+        count += 1
+    else:
+        messagebox.showerror("Tic Tac Toe", "Hey! That box has already been selected\nPick Another Box...")
 
 
 def buttons():
+    global clicked, count
+    clicked = True
+    count = 0
+
     entry_list = ''
 
     for entries in my_entries:
@@ -24,10 +43,11 @@ def buttons():
 
 
 # Row Loop
+
 for y in range(w):
     # Column Loop
     for x in range(w):
-        my_entry = Button(root, text="ciao", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+        my_entry = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
                           command=lambda: b_click(my_entry))
         my_entry.grid(row=y, column=x)
         my_entries.append(my_entry)

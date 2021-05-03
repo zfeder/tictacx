95515b36d10a3c2048dd74060332571c8fbc1e4c
Giorgio <giorgio.licitra264@edu.unito.it>
2021-05-03 16:15:52 +0200

    matrix button work

--- TicTacx/main.py
+++ TicTacx/main.py
@@ -1,55 +1,44 @@
-from tkinter import *
-from tkinter import messagebox
-
-root = Tk()
-root.title("Filetto")
-root.geometry("700x700")
-
-clicked = True
-count = 0
-
-my_entries = []
-
-z = input("Inserisci il valore: ")
-w = int(z)
+import tkinter as tk
 
 
-def b_click(b):
-    global clicked, count
+# --- functions ---
 
-    if b["text"] == " " and clicked == True:
-        b["text"] = "X"
-        clicked = False
-        count += 1
-    elif b["text"] == " " and clicked == False:
-        b["text"] = "O"
-        clicked = True
-        count += 1
-    else:
-        messagebox.showerror("Tic Tac Toe", "Hey! That box has already been selected\nPick Another Box...")
+def on_click(widget, y, x, A):
+    print('clicked')
+    widget['text'] = 1
+    A[y][x] = "1"
+    print(A)
 
 
-def buttons():
-    global clicked, count
-    clicked = True
-    count = 0
-
-    entry_list = ''
-
-    for entries in my_entries:
-        entry_list = entry_list + str(entries.get()) + '\n'
-
-    print(my_entries[24].get())
+def rigaCall():
+    riga = input("Scegli la riga:");
+    nRiga = int(riga);
+    return nRiga;
 
 
-# Row Loop
+def colonnaCall():
+    colonna = input("Scegli la colonna:");
+    nColonna = int(colonna);
+    return nColonna;
 
-for y in range(w):
-    # Column Loop
-    for x in range(w):
-        my_entry = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
-                          command=lambda: b_click(my_entry))
-        my_entry.grid(row=y, column=x)
-        my_entries.append(my_entry)
+
+# --- main ---
+
+
+root = tk.Tk()
+lato = input('Inserisci la grandezza della griglia: ')
+print(lato);
+nLato = int(lato);
+Giocatori = input('Inserisci il numero di giocatori: ')
+nGiocatori = int(Giocatori);
+print(nGiocatori);
+
+A = [[0 for x in range(nLato)], [0 for x in range(nLato)]]
+
+for y in range(nLato):
+    for x in range(nLato):
+        button = tk.Button(root, text="0")
+        button['command'] = lambda arg=button: on_click(arg, y, x, A)
+        button.grid(row=y, column=x)
 
 root.mainloop()

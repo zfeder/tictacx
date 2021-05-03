0e077f380a04866221bc1765aced2879b97bf0a6
Giorgio <giorgio.licitra264@edu.unito.it>
2021-05-03 18:38:23 +0200

    turni

--- TicTacx/main.py
+++ TicTacx/main.py
@@ -1,13 +1,35 @@
 import tkinter as tk
-
+from tkinter import font as tkFont
 
 # --- functions ---
+import numpy as np
 
-def on_click(widget, y, x, A):
+
+def on_click(widget, x, y):
     print('clicked')
-    widget['text'] = 1
-    A[y][x] = "1"
-    print(A)
+    global parziale
+    if A[x][y] == 0:
+        if parziale != nGiocatori:
+            widget['text'] = nGiocatori - (parziale - 1)
+            A[x][y] = nGiocatori - (parziale - 1)
+            print(A)
+            print("Giocatore" +(nGiocatori - (parziale - 1))+ "è il tuo turno")
+            parziale = parziale - 1
+        if parziale == nGiocatori:
+            widget['text'] = 1
+            A[x][y] = 1
+            print(A)
+            print("Giocatore 1 è il tuo turno")
+            parziale = parziale - 1
+        if parziale == 0:
+            widget['text'] = nGiocatori
+            A[x][y] = nGiocatori
+            print(A)
+            print("Giocatore" +nGiocatori+ "è il tuo turno")
+            parziale = nGiocatori
+
+    else:
+        print('Casella già occupata')
 
 
 def rigaCall():
@@ -27,18 +49,21 @@ def colonnaCall():
 
 root = tk.Tk()
 lato = input('Inserisci la grandezza della griglia: ')
-print(lato);
-nLato = int(lato);
+print(lato)
+nLato = int(lato)
 Giocatori = input('Inserisci il numero di giocatori: ')
-nGiocatori = int(Giocatori);
-print(nGiocatori);
+nGiocatori = int(Giocatori)
+print(nGiocatori)
+parziale = nGiocatori
 
-A = [[0 for x in range(nLato)], [0 for x in range(nLato)]]
+A = np.zeros((nLato, nLato))
 
 for y in range(nLato):
     for x in range(nLato):
-        button = tk.Button(root, text="0")
-        button['command'] = lambda arg=button: on_click(arg, y, x, A)
+        helv36 = tkFont.Font(family='Helvetica', size=20, weight=tkFont.BOLD)
+        button = tk.Button(root, font=helv36, height=5, width=10, text="0")
+        button['command'] = lambda x=x, y=y, arg=button: on_click(arg, y, x)
         button.grid(row=y, column=x)
 
+print("Giocatore 1 è il tuo turno")
 root.mainloop()
